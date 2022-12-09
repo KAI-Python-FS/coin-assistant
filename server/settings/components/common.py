3c69699bbac237e5931104e8b94f0653bbf293b1
Maksimyuk <georgiy.maksimyuk@flatstack.dev>
2022-12-09 14:38:14 +0300

    * Добавление миграций

--- server/settings/components/common.py
+++ server/settings/components/common.py
@@ -24,6 +24,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'server.app.users',
+    'server.app.purchases',
 ]
 
 MIDDLEWARE = [
