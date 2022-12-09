a884a6d80a2d26f769ec6b52ff16354b7f7071f4
Maksimyuk <georgiy.maksimyuk@flatstack.dev>
2022-12-09 14:07:42 +0300

    * Добавление модели Покупки

--- server/app/purchases/models.py
+++ server/app/purchases/models.py
@@ -1,3 +1,35 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+
+User = get_user_model()
+
+
+class Purchase(models.Model):
+    """Модель покупки"""
+
+    name = models.TextField(
+        verbose_name="Наименование покупки",
+    )
+    description = models.TextField(
+        null=True,
+        verbose_name="Описание покупки",
+    )
+    purchased_at = models.DateTimeField(
+        verbose_name="Дата покупки",
+    )
+    cost = models.FloatField(
+        verbose_name="Стоимость",
+    )
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        verbose_name="Покупатель",
+    )
+
+    class Meta:
+        verbose_name = 'Покупка'
+        verbose_name_plural = 'Покупки'
+
+    def __str__(self):
+        return f'{self.user}-at-{self.purchased_at}'
