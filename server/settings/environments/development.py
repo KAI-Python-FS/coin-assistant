52005a193bfe604ed8f2fde8a643e3a872e35bf8
Maksimyuk <georgiy.maksimyuk@flatstack.dev>
2022-12-09 11:33:02 +0300

    * донастройка сетапа проекта

---
+++ server/settings/environments/development.py
@@ -0,0 +1,44 @@
+"""
+This file contains all the settings that defines the development server.
+
+SECURITY WARNING: don't run with debug turned on in production!
+"""
+
+from typing import List
+
+from server.settings.components.common import (
+    ALLOWED_HOSTS,
+    DATABASES,
+    INSTALLED_APPS,
+    MIDDLEWARE,
+)
+
+# Setting the development status:
+
+DEBUG = True
+
+ALLOWED_HOSTS += [
+    'localhost',
+    '0.0.0.0',  # noqa: S104
+    '127.0.0.1',
+    '[::1]',
+    '29d1-94-180-176-227.ngrok.io',
+]
+
+
+# Installed apps for development only:
+
+INSTALLED_APPS += (
+    # Better debug:
+    # 'debug_toolbar',
+)
+
+
+# Static files:
+# https://docs.djangoproject.com/en/3.2/ref/settings/#std:setting-STATICFILES_DIRS
+
+STATICFILES_DIRS: List[str] = []
+
+# Disable persistent DB connections
+# https://docs.djangoproject.com/en/3.2/ref/databases/#caveats
+DATABASES['default']['CONN_MAX_AGE'] = 0
