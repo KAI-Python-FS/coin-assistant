1698db8b0f72681de8a0fa76b67b82495c0a3725
Maksimyuk <georgiy.maksimyuk@flatstack.dev>
2022-12-09 15:36:14 +0300

    * Админка

--- server/app/purchases/admin.py
+++ server/app/purchases/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 
 from server.app.purchases import models
 
-# Register your models here.
+
 admin.site.register(models.Category)
 admin.site.register(models.Purchase)
