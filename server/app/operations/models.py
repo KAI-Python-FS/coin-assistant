10016f4c0998e81226b585041e19e12166f428fd
Maksimyuk <georgiy.maksimyuk@flatstack.dev>
2022-12-13 09:44:16 +0300

    * Перенос моделей оплаты в апп операций

--- server/app/operations/models.py
+++ server/app/operations/models.py
@@ -1,3 +1,56 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+
+User = get_user_model()
+
+
+class Category(models.Model):
+    """Модель категории покупки"""
+
+    name = models.CharField(
+        max_length=64,
+        verbose_name="Название категории"
+    )
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+    def __str__(self):
+        return f'{self.name}'
+
+
+class Purchase(models.Model):
+    """Модель покупки"""
+
+    name = models.TextField(
+        verbose_name="Наименование покупки",
+    )
+    description = models.TextField(
+        null=True,
+        verbose_name="Описание покупки",
+    )
+    purchased_at = models.DateTimeField(
+        verbose_name="Дата покупки",
+    )
+    cost = models.FloatField(
+        verbose_name="Стоимость",
+    )
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        verbose_name="Покупатель",
+    )
+    category = models.ManyToManyField(
+        Category,
+        null=True,
+        verbose_name="Категория покупки",
+    )
+
+    class Meta:
+        verbose_name = 'Покупка'
+        verbose_name_plural = 'Покупки'
+
+    def __str__(self):
+        return f'{self.user}-at-{self.purchased_at}'
