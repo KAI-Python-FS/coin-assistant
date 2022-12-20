c584fba1c3c282fc0c4a7a66f3d141f66823a50e
Maksimyuk <georgiy.maksimyuk@flatstack.dev>
2022-12-20 22:59:01 +0300

    (#issue-3) * Настройка возможности выполнения запросов используя UI swagger

--- server/settings/components/common.py
+++ server/settings/components/common.py
@@ -26,6 +26,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rest_framework',
+    'corsheaders',
     'rest_framework.authtoken',
 
     'server.app.goals',
@@ -41,6 +42,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'server.urls'
