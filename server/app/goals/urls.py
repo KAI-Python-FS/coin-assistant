8cdb2e71a022c4eac15f2c5889e2dbd069adb6da
Maksimyuk <georgiy.maksimyuk@flatstack.dev>
2022-12-22 17:26:16 +0300

    (#issue-4) * актуализация УРЛов

--- server/app/goals/urls.py
+++ server/app/goals/urls.py
@@ -5,9 +5,10 @@ from . import apis
 
 goal_accumulation_patterns = [
     path("", apis.GoalRefillGeneralView.as_view(), name="goal-refill"),
-    path("<int:goal_id>", apis.GoalRefillConcreteView.as_view(), name="concrete-goal-refill")
+    path("<int:goal_id>", apis.GoalRefillConcreteView.as_view(), name="concrete-goal-refill"),
 ]
 
 budget_patterns = [
     path("", apis.BudgetGeneralView.as_view(), name="budget"),
+    path("<int:budget_id>", apis.BudgetConcreteView.as_view(), name="concrete-budget"),
 ]
