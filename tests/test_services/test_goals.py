9dec1a485903bfcca2f2d070fd6f215f149ad82b
Maksimyuk <georgiy.maksimyuk@flatstack.dev>
2022-12-23 17:51:54 +0300

    (#issue-29)
    * Фикс фабрики и теста

--- tests/test_services/test_goals.py
+++ tests/test_services/test_goals.py
@@ -1,5 +1,42 @@
-# class TestGoalRefillService:
-#     """Проверка основных методов сервиса"""
-#
-#     def test_create(self):
-#         """Тест проверки создания цели накопления"""
+
+import pytest
+
+from server.app.goals.services import GoalRefillService
+from server.app.goals.models import Goal
+from server.app.goals.enums import GoalRefillRuleEnum
+from tests.factories.user import UserFactory
+
+
+TEST_DATA_SERVICE_CREATE = [
+    ("Тест", None, None, None, None, None, 1, GoalRefillRuleEnum.eq, )
+    # pytest.param("6*9", 42, marks=pytest.mark.xfail)
+]
+
+
+class TestGoalRefillService:
+    """Проверка основных методов сервиса"""
+
+    @pytest.mark.parametrize(
+        "name, description, category, start_date, finish_date, state, value, rule",
+        TEST_DATA_SERVICE_CREATE,
+    )
+    def test_create(
+        self,
+        name,
+        description,
+        category,
+        start_date,
+        finish_date,
+        state,
+        value,
+        rule,
+    ):
+        """Тест проверки создания цели накопления"""
+        user = UserFactory()
+
+        service = GoalRefillService(user=user)
+        result = service.create(
+            name, description, category, start_date, finish_date, state, value, rule
+        )
+
+        assert Goal.objects.
