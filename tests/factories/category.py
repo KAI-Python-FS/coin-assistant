5bbc6c8848edf95acbc5cf6fca5c648f1d320167
Maksimyuk <georgiy.maksimyuk@flatstack.dev>
2022-12-23 16:21:23 +0300

    (#issue-29)
    * Добавление Фабрик

---
+++ tests/factories/category.py
@@ -0,0 +1,14 @@
+import factory
+
+from factory.django import DjangoModelFactory
+
+from server.app.operations.models import Category
+
+
+class CategoryFactory(DjangoModelFactory):
+    """Фабрика создания категорий"""
+
+    name = factory.Sequence(lambda n: f'name_{n}')
+
+    class Meta:
+        model = Category
---
+++ tests/factories/user.py
@@ -0,0 +1,19 @@
+import factory
+
+from django.contrib.auth import get_user_model
+from django.contrib.auth.hashers import make_password
+from factory.django import DjangoModelFactory
+
+User = get_user_model()
+
+
+class UserFactory(DjangoModelFactory):
+    """Фабрика создания пользователей"""
+
+    first_name = factory.Faker('first_name')
+    last_name = factory.Faker('last_name')
+    username = factory.Faker('email')
+    password = factory.LazyFunction(lambda: make_password('pi3.1415'))
+
+    class Meta:
+        model = User
