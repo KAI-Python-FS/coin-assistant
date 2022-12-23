463a095245115154f55a522131e3712f38c1b30c
Maksimyuk <georgiy.maksimyuk@flatstack.dev>
2022-12-23 17:01:09 +0300

    (#issue-29)
    * Первый тест

---
+++ tests/unittests/managers/goals.py
@@ -0,0 +1,17 @@
+from server.app.goals.models import Goal
+from tests.factories.goals import BudgetFactory, GoalRefillFactory
+
+
+class TestGoalManager:
+    """Проверка работы менеджера"""
+
+    def test_goal_queryset(self):
+        """Тест проверки получения целей накопления"""
+        budgets, goal_refills = (
+            BudgetFactory.create_batch(size=3),
+            GoalRefillFactory.create_batch(size=4),
+        )
+
+        actual_goals = Goal.objects.goals().count()
+
+        assert actual_goals == len(goal_refills)
