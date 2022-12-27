aa49c6ab37759461ead30eb78bd27ba7b958c028
Maksimyuk <georgiy.maksimyuk@flatstack.dev>
2022-12-27 11:51:19 +0300

    (#issue-29)
    * Написание теста Создания категории

---
+++ tests/test_services/test_categories.py
@@ -0,0 +1,37 @@
+
+from typing import Any
+
+import pytest
+
+from server.app.operations.models import Category
+from server.app.operations.services import CategoryService
+
+
+class TestCategoryService:
+    """Проверка основных методов сервиса"""
+
+    @pytest.mark.django_db()
+    @pytest.mark.parametrize(
+        "create_params",
+        [
+            pytest.param(
+                {
+                    "name": None,
+                },
+                marks=pytest.mark.xfail,
+            ),
+            pytest.param(
+                {
+                    "name": "Категория123",
+                },
+            ),
+        ]
+    )
+    def test_create(
+        self,
+        create_params: dict[str, Any],
+    ):
+        service = CategoryService()
+        _ = service.create(**create_params)
+
+        assert Category.objects.count() == 1
