ea8b390a573d48578913965e86f1915f5c3f5816
Maksimyuk <georgiy.maksimyuk@flatstack.dev>
2022-12-27 12:08:55 +0300

    (#issue-29)
    * Тест получения списка категорий

--- tests/test_services/test_categories.py
+++ tests/test_services/test_categories.py
@@ -5,6 +5,7 @@ import pytest
 
 from server.app.operations.models import Category
 from server.app.operations.services import CategoryService
+from tests.factories.category import CategoryFactory
 
 
 class TestCategoryService:
@@ -36,3 +37,12 @@ class TestCategoryService:
         _ = service.create(**create_params)
 
         assert Category.objects.count() == 1
+
+    @pytest.mark.django_db()
+    def test_retrieve_list(self):
+        """Тест проверки получения данных категорий"""
+        _ = CategoryFactory.create_batch(3)
+
+        service = CategoryService()
+
+        assert service.retrieve_list().count() == 3
