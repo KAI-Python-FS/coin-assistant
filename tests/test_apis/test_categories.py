661d60e9a9ccde77d4d32d190353c28556122138
Maksimyuk <georgiy.maksimyuk@flatstack.dev>
2022-12-29 12:01:39 +0300

    (#issue-30)
    * Тест создания Категорий

--- tests/test_apis/test_categories.py
+++ tests/test_apis/test_categories.py
@@ -3,6 +3,7 @@ import json
 
 import pytest
 
+from server.app.operations.serializers import CategoryCreateInputSerializer, CategoryCreateOutputSerializer
 from tests.factories.category import CategoryFactory
 from tests.factories.user import UserFactory
 
@@ -22,4 +23,21 @@ class TestCategoryEndpoints:
         assert response.status_code == 200
         assert len(json.loads(response.content)) == 3
 
+    @pytest.mark.django_db(reset_sequences=True)
+    def test_create(self, api_client_authorized):
+        """Проверка создания категории"""
+        input_params = {
+            "name": "Тест",
+        }
 
+        response = api_client_authorized.post(
+            self.endpoint,
+            data=input_params,
+            format="json",
+        )
+
+        assert response.status_code == 201
+        assert json.loads(response.content) == {
+            "id": 1,
+            "name": input_params["name"],
+        }
