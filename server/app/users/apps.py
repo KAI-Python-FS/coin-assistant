c6fade904743ae0bb2d3356dad80ff7bcb6fe383
Maksimyuk <georgiy.maksimyuk@flatstack.dev>
2022-12-29 11:41:40 +0300

    (#issue-30)
    * фикс автогенерации Токена авторизации пользователя

--- server/app/users/apps.py
+++ server/app/users/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class UsersConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'server.app.users'
+
+    def ready(self):
+        from .receivers import create_auth_token
