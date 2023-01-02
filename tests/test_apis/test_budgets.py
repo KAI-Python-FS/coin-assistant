f159eabcf8c66ff0a39e500f15ae60724e4b361f
Maksimyuk <georgiy.maksimyuk@flatstack.dev>
2023-01-02 21:22:57 +0300

    (#issue-30)
    * Добавление теста создания Бюджета пользователя

--- tests/test_apis/test_budgets.py
+++ tests/test_apis/test_budgets.py
@@ -3,6 +3,7 @@ import json
 
 import pytest
 
+from server.app.goals.enums import BudgetRuleEnum, GoalStateEnum
 from tests.factories.goals import BudgetFactory
 
 
@@ -20,3 +21,36 @@ class TestBudgetEndpoints:
 
         assert response.status_code == 200
         assert len(json.loads(response.content)) == 3
+
+    @pytest.mark.django_db(reset_sequences=True)
+    @pytest.mark.parametrize(
+        "create_params",
+        [
+            pytest.param(
+                {
+                    "name": "тестовое имя",
+                    "value": 2,
+                    "state": GoalStateEnum.unknown,
+                    "rule": BudgetRuleEnum.eq,
+                },
+            ),
+        ]
+    )
+    def test_create(self, api_client_authorized, create_params: dict):
+        """Проверка создания Бюджета"""
+        expected = {
+            "id": 1,
+            **create_params,
+        }
+
+        response = api_client_authorized.post(
+            self.endpoint,
+            data=create_params,
+            format="json",
+        )
+
+        assert response.status_code == 201
+        # Проверка наличия переданных значений в ответе
+        response_as_json = json.loads(response.content)
+        for each_create_param_name, each_create_param_value in expected.items():
+            assert response_as_json[each_create_param_name] == each_create_param_value
